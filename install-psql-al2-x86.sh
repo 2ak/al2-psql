7c9118871dab19c60d5cfb3edada1be99dce6df3
Andy Katz <31285319+2ak@users.noreply.github.com>
2021-03-20 11:34:46 -0700

    Add files via upload

---
+++ install-psql-al2-x86.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+sudo tee /etc/yum.repos.d/pgdg.repo<<EOF
+[pgdg13]
+name=PostgreSQL 13 for RHEL/CentOS 7 - x86_64
+baseurl=https://download.postgresql.org/pub/repos/yum/13/redhat/rhel-7-x86_64
+enabled=1
+gpgcheck=0
+EOF
+
+sudo yum -y install postgresql13
+
